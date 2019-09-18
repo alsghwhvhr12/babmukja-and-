c250fad47a562599615efa3d58c7d879c4ae9d03
배진우 <bjwkor@naver.com>
2019-09-18 23:48:38 +0900

    회원가입

---
+++ app/src/main/java/com/example/samplesenti/RegActivity.java
@@ -0,0 +1,31 @@
+package com.example.samplesenti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+
+        EditText etId  = (EditText) findViewById(R.id.etId);
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        EditText etName = (EditText) findViewById(R.id.etName);
+        Button btnReg= (Button) findViewById(R.id.btnReg);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerintent = new Intent(RegActivity.this,MainMenuAct.class);
+                RegActivity.this.startActivity(registerintent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/samplesenti/loginAct.java
+++ app/src/main/java/com/example/samplesenti/loginAct.java
@@ -4,13 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.widget.EditText;
 
 public class loginAct extends AppCompatActivity {
 
@@ -19,16 +15,28 @@ public class loginAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btn_go = (Button) findViewById(R.id.button);
+        EditText etId  = (EditText) findViewById(R.id.etId);
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        Button btnReg= (Button) findViewById(R.id.btnReg);
+        Button btn_go = (Button) findViewById(R.id.btnLogin);
+
         btn_go.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v) {
                         //로그인화면
-                        Intent intent = new Intent(getApplicationContext(),MainMenuAct.class);
+                        Intent intent = new Intent(getApplicationContext(), MainMenuAct.class);
                         //액티비티 시작
                         startActivity(intent);
                     }
-                }
-        );
+                });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerintent = new Intent(loginAct.this, RegActivity.class);
+                loginAct.this.startActivity(registerintent);
+            }
+        });
+
     }
 }
