a8ad74117284af31ac89c2bd414cc395033e0fdd
배진우 <bjwkor@naver.com>
2019-09-24 23:24:41 +0900

    회원가입(php,RegActivity,RegRequsest) PHP연동 해야함 테스트해봐야함. 오류뜨면 빌드SDK최신으로 바꾸세요
    9/24 23:22
    php 회원가입 넘어가는거 확인 수정할거많음 아직

--- app/src/main/java/com/example/samplesenti/RegActivity.java
+++ app/src/main/java/com/example/samplesenti/RegActivity.java
@@ -1,13 +1,14 @@
 package com.example.samplesenti;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -23,54 +24,48 @@ public class RegActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
 
-        final EditText etId  = (EditText) findViewById(R.id.etId);
+        final EditText etId = (EditText) findViewById(R.id.etId);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final EditText etName = (EditText) findViewById(R.id.etName);
         final EditText etPhone = (EditText) findViewById(R.id.etPhone);
-        Button btnReg= (Button) findViewById(R.id.btnReg);
+        final Button btnReg;
 
-        btnReg.setOnClickListener(new View.OnClickListener(){
+        btnReg = findViewById(R.id.btnReg);
+        btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                String id=etId.getText().toString();
-                String pw=etPassword.getText().toString();
-                String tel=etPhone.getText().toString();
-                String name=etName.getText().toString();
+            public void onClick(View view) {
+                String id = etId.getText().toString();
+                String password = etPassword.getText().toString();
+                String name = etName.getText().toString();
+                String tel = etPhone.getText().toString();
 
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                      try{
-                          JSONObject jsonResponse = new JSONObject(response);
-                          boolean success=jsonResponse.getBoolean("success");
-                          if(success){
-                              AlertDialog.Builder builder = new AlertDialog.Builder(RegActivity.this);
-                          builder.setMessage("등록 성공");
-                          builder.setPositiveButton("확인",null);
-                          builder.create()
-                                  .show();
-                          Intent intent = new Intent(RegActivity.this,loginAct.class);
-                          RegActivity.this.startActivity(intent);
-                          }
-                          else
-                          {
-                              AlertDialog.Builder builder = new AlertDialog.Builder(RegActivity.this);
-                              builder.setMessage("등록 실패");
-                              builder.setNegativeButton("실패",null);
-                              builder.create()
-                                      .show();
-                          }
-                      }
-                      catch (JSONException e)
-                      {
-                          e.printStackTrace();
-                      }
+            Response.Listener<String> responseListener = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        boolean success = jsonObject.getBoolean("success");
+                        if (success) {
+                            Toast.makeText(RegActivity.this,"회원등록에 성공 하셨습니다.",Toast.LENGTH_SHORT).show();
+                            Intent intent =new Intent(RegActivity.this,loginAct.class);
+                            startActivity(intent);
+                        }
+                        else
+                            {
+                                Toast.makeText(RegActivity.this,"회원등록에 실패 하셨습니다.",Toast.LENGTH_SHORT).show();
+                                return;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
+                }
+            };
+
+                    //서버로 Volley를 이용해 요청
+                    RegRequest registerRequest = new RegRequest(id,password,name,tel,responseListener);
+                    RequestQueue queue =Volley.newRequestQueue(RegActivity.this);
+                    queue.add(registerRequest);
                 };
-                RegRequest regRequest = new RegRequest(id,pw,name,tel,responseListener);
-                RequestQueue queue = Volley.newRequestQueue(RegActivity.this);
-                queue.add(regRequest);
-            }
-        });
+            });
+        }
     }
-}
--- app/src/main/java/com/example/samplesenti/RegRequest.java
+++ app/src/main/java/com/example/samplesenti/RegRequest.java
@@ -1,5 +1,7 @@
 package com.example.samplesenti;
 import java.util.*;
+
+import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 import java.util.Map;
@@ -10,17 +12,18 @@ public class RegRequest extends  StringRequest {
 final static private  String URL= "http://babmukja.pe.kr/reg.php";
 private Map<String,String> parameters;
 
-public RegRequest(String id, String pw, String name, String tel , Response.Listener<String> listener)
+public RegRequest(String id, String password, String name, String tel , Response.Listener<String> listener)
 {
     super(Method.POST,URL,listener,null);
     parameters = new HashMap<>();
     parameters.put("id",id);
-    parameters.put("pw",pw);
+    parameters.put("password",password);
     parameters.put("name",name);
     parameters.put("tel",tel);
 }
 @Override
-public Map<String,String> getParams(){
+public Map<String,String> getParams() throws AuthFailureError
+{
     return parameters;
 }
 }
